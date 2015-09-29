fc1ef4d605389baa3e58ecddfee8b1a224ee28e1
jmitsakos <jmitsakos@gmail.com>
2015-09-29 13:34:23 +0300

    Remove unnecessary file from git

--- presentation/templates_c/%%9A^9A2^9A2B2B5B%%blank.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2015-09-14 20:38:40
-         compiled from blank.tpl */ ?>
--- presentation/templates_c/%%B3^B32^B32E467B%%not_authorized.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2015-09-14 22:12:52
-         compiled from not_authorized.tpl */ ?>
-<h1> Δεν έχετε δικαίωμα πρόσβασης σε αυτή τη σελίδα </h1>
